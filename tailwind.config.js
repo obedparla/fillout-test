9dd479c684b48042bef9f7ee058b30c8b14e9b8d
obedmarquez <obed.imp@gmail.com>
2025-07-18 17:36:40 +0200

    chore: add tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    // Normally I'd add a theme here to use all the app's colors if they differ from Tailwind's
+    extend: {},
+  },
+  plugins: [],
+};
